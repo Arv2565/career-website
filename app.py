f51c2f1d22b342b667c076a8dfb8d5474dab5ba3
Arv2565 <117474111+Arv2565@users.noreply.github.com>
2023-10-09 12:46:26 +0000

    added HTML and CSS

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app= Flask(__name__)
 @app.route("/") # / refers to the empty route
 def hello_world():
-  return "hello world!"
+  return render_template('home.html')
 
 if __name__=="__main__":
   app.run(host='0.0.0.0', debug=True)#in order to make the local computer as a host and debug for dynamic rendering
